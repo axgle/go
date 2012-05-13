0e5ac85704f516c6d4287f0fc88ac2c49bb2dacf
haoqis <haoqis@gmail.com>
2012-05-14 00:23:16 +0800

    '.'

--- php54_webserver.go
+++ php54_webserver.go
@@ -11,6 +11,11 @@ func main(){
   // >=php5.4 buildin web server
   cmd := "php -S localhost:"+os.Args[1] 
   cmd_fields:=strings.Fields(cmd)
+  
+//notice
+//d:=(strings.Fields(`a "th is" c`))
+//fmt.Printf("%d %s",len(d),d[1])//notice: d[1] != "this is"
+
   go func(){
    exec.Command(cmd_fields[0],cmd_fields[1:]...).Run()
   }()
