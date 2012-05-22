d6bade30cac3f2a98087fdbfea6d2b3687c30a13
haoqis <haoqis@gmail.com>
2012-05-22 09:24:50 +0800

    '.'

--- cmd_gbk.go
+++ cmd_gbk.go
@@ -1,7 +1,7 @@
 // display gbk/gb2312 Chinese chars on window's cmd which default using gbk encoding
 package main
 import "fmt"
-import "code.google.com/p/mahonia"
+import "github.com/axgle/mahonia"
 func main(){
   enc:=mahonia.NewEncoder("gbk")
   fmt.Println(enc.ConvertString("hello,世界"))  
