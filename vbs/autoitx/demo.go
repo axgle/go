fa8a3178e4395cfc421408b641ecf1620f2684c0
haoqis <haoqis@gmail.com>
2012-06-01 00:42:00 +0800

    .

--- vbs/autoitx/demo.go
+++ vbs/autoitx/demo.go
@@ -6,17 +6,20 @@ package main
 #cgo LDFLAGS:./AutoItX3.dll
 void run(){
   AU3_Run(L"notepad.exe",L"",1);
+  AU3_Sleep(500);
+  AU3_WinActivate(L"无标题 - 记事本",L"");
+  AU3_Send(L"test{enter}",0);
 }
 */
 import "C"
 import(
- "fmt"
+ //"fmt"
  //"syscall"
 )
 func main(){
   C.run()
  
- fmt.Printf("hi")
+ //fmt.Printf("hi")
 
 }
  
