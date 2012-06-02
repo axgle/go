19c9f6877c3734e050fe59c338de5f44d67ff0cb
haoqis <haoqis@gmail.com>
2012-06-02 18:05:56 +0800

    .

--- leveldb/memdb.go
+++ leveldb/memdb.go
@@ -2,15 +2,31 @@ package main
 import(
  "fmt"
  "os"
+ "code.google.com/p/leveldb-go/leveldb/memdb"
  "code.google.com/p/leveldb-go/leveldb/table"
   //"code.google.com/p/leveldb-go/leveldb/db"
  //"code.google.com/p/leveldb-go/leveldb"
 )
+
+func p(r []byte,e error){
+  println(string(r))
+}
 func main(){
+ 
+  db:=memdb.New(nil)
+  db.Set([]byte("name"),[]byte("axgle"),nil)
+  
+  p( db.Get([]byte("name") ,nil) )
+  
+  
+}
+func main2(){
   
   f,_:=os.Create("a.db")
   t:=table.NewWriter(f,nil)
  t.Set([]byte("google"),[]byte("yes!"),nil)
+ 
+ 
  t.Close()
  
  f,_ =os.Open("a.db")
